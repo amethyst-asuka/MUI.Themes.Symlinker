5085ee05fb9e7bdab33e0760f3772264105f4751
ameth <ameth@DESKTOP-5L1QPEN>
2016-06-21 16:40:45 +0800

    vb source code

---
+++ MUI.Themes.Symlinker/HardLink.vb
@@ -0,0 +1,10 @@
+Imports System.Runtime.InteropServices
+
+''' <summary>
+''' Provides access to NTFS hard links in .Net.
+''' </summary>
+Public Module HardLink
+    <DllImport("Kernel32.dll", CharSet:=CharSet.Unicode)>
+    Public Function CreateHardLink(lpFileName As String, lpExistingFileName As String, lpSecurityAttributes As IntPtr) As Boolean
+    End Function
+End Module
